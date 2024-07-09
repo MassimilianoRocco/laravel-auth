af1fdd58259ddd413646c7056a0090d95d05c6ba
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-07-09 17:33:17 +0200

    creo create

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -28,7 +28,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //////////////////////////////////////////////////
+        return view('projects.create');
     }
 
     /**
