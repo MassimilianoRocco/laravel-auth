1ec05bebd491363533c5b4dd1a0bc11d8f508436
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-09 17:06:24 +0200

    Add columns

--- database/migrations/2024_07_09_142244_create_projects_table.php
+++ database/migrations/2024_07_09_142244_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            $table->string('titolo');
+            $table->string('descrizione');
+            $table->string('immagine');
+
             $table->timestamps();
         });
     }
